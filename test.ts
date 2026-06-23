9cc97f6b13d54ab75677c65906ade2206507f916
Sebastian <sebastian19armas@gmail.com>
2026-06-23 17:42:24 -0400

    Dinow Version 1.2

--- test.ts
+++
@@ -1,8 +0,0 @@
-import { db } from "./lib/db/index";
-import { deberes } from "./lib/db/schema";
-async function main() {
-  const all = await db.select().from(deberes);
-  console.log(JSON.stringify(all, null, 2));
-  process.exit(0);
-}
-main();
